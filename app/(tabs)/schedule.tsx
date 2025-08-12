e5f3451cb1fa97c54289ad40a255f4dacb356956
Nicholas Wong <Nickstorm07@gmail.com>
2025-08-11 20:27:59 -0700

    refactored plans and add workout

--- app/(tabs)/schedule.tsx
+++ app/(tabs)/schedule.tsx
@@ -1,7 +1,7 @@
 import EventDetailCard from '@/components/EventDetailCard';
 import EventModalComponent from '@/components/eventModal';
+import { useWorkout } from '@/components/plans/WorkoutContext';
 import { ThemedView } from '@/components/ThemedView';
-import { useWorkout } from '@/components/WorkoutContext';
 import React, { useMemo, useState } from 'react';
 import { Dimensions, StyleSheet, View } from 'react-native';
 import { Calendar } from 'react-native-big-calendar';
